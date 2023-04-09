fadd48693d57c53bda6f7665bdf82b4f78461b30
dark-king-001 <piyushat115@gmail.com>
2023-04-09 15:44:01 +0530

    download working with subprocess scheduler

--- download.py
+++ download.py
@@ -1,8 +1,11 @@
-# import Worker as wk
-import os
-download_server_connections = 8
-print(f'server conncetions opened : {download_server_connections}')
+import Worker as wk
 chunk_size = 100 #mbs
-print(f'chunk_size : {chunk_size}')
-# wk.Worker('/home/piyush/Desktop/SystemControl/download_server/download/aria2c.exe' + f'-x{download_server_connections} ' + f' -k{chunk_size}M ' + ' ' + input('Enter Download Link : '))
-os.system('aria2c' + f' -x{download_server_connections} ' + f' -k{chunk_size}M ' + ' ' + input('Enter Download Link : '))
+
+# In this command:
+# -x8, -s8, and -j8 are options that specify the number of connections to be used for downloading the file. In this case, we're using 8 connections for each server, and the maximum number of connections is also set to 8.
+# --split=8 and --min-split-size=100M are options that control the size of the individual chunks of the file that are downloaded simultaneously. In this case, we're using 8 chunks of at least 100 MB each.
+# --dir=/path/to/download/location is an option that specifies the directory where the downloaded file should be saved.
+# "http://example.com/file.zip" is the URL of the file to be downloaded.
+
+def load(path, link):
+    wk.Worker('aria2c -x8 -s8 -j8 --split=8 --min-split-size=100M ' + f'--dir={path} '+ link)

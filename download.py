3bffb76a7c64313464b455292b216e2e7c70e8a9
dark-king-001 <piyushat115@gmail.com>
2023-04-09 23:14:00 +0530

    minor bug fixes

--- download.py
+++ download.py
@@ -2,6 +2,8 @@ import Worker as wk
 import sys
 chunk_size = 100 #mbs
 
+# python3 download.py <path> <link>
+
 # In this command:
 # -x8, -s8, and -j8 are options that specify the number of connections to be used for downloading the file. In this case, we're using 8 connections for each server, and the maximum number of connections is also set to 8.
 # --split=8 and --min-split-size=100M are options that control the size of the individual chunks of the file that are downloaded simultaneously. In this case, we're using 8 chunks of at least 100 MB each.
@@ -9,4 +11,4 @@ chunk_size = 100 #mbs
 # "http://example.com/file.zip" is the URL of the file to be downloaded.
 
 def load(path, link):
-    wk.Worker('aria2c -x8 -s8 -j8 --split=8 --min-split-size=100M ' + f'--dir={path} '+ sys.argv[1])
+    wk.Worker('aria2c -x8 -s8 -j8 --split=8 --min-split-size=100M ' + f'--dir='+ sys.argv[1] + ' ' + sys.argv[2])

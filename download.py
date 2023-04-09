8567270add4f68e2fdcc326935bc35ef6baedd6c
dark-king-001 <piyushat115@gmail.com>
2023-04-09 15:23:14 +0530

    the package given with this repository can only be used for windows due to binary coding and for linux aria2c is primarily available with apt-get. thus currently removing the windows client from the repository. the following command on linux can correctly install aria2c <sudo apt-get install aria2>. updating script to work without workers for now

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
-import Worker as wk
+# import Worker as wk
+import os
 download_server_connections = 8
 print(f'server conncetions opened : {download_server_connections}')
 chunk_size = 100 #mbs
 print(f'chunk_size : {chunk_size}')
-wk.Worker('aria2/aria2c.exe' + f'-x{download_server_connections} ' + f' -k{chunk_size}M ' + ' ' + input('Enter Download Link : '))
+# wk.Worker('/home/piyush/Desktop/SystemControl/download_server/download/aria2c.exe' + f'-x{download_server_connections} ' + f' -k{chunk_size}M ' + ' ' + input('Enter Download Link : '))
+os.system('aria2c' + f' -x{download_server_connections} ' + f' -k{chunk_size}M ' + ' ' + input('Enter Download Link : '))
